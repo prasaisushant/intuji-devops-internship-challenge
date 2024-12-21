367329ba058c4a3eba0f83addae253594b57bc14
Sushant Prasai <157332940+prasaisushant@users.noreply.github.com>
2024-12-21 07:36:20 +0545

    Update HelloWorldTest.php

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -5,6 +5,7 @@
  * (c) Guillaume Sainthillier <hello@silarhi.fr>
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
+ * test
  */
 
 namespace Silarhi\Tests;
